cf46272324f1b3df433dc9a29845b51b19fa706b
Erik Reinert <e@drn.io>
2020-03-22 15:42:23 -0700

    #3: Implements sync AppRoles and rendering to FS for external use

--- main.go
+++ main.go
@@ -1,6 +1,12 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+
 	"github.com/sirupsen/logrus"
 )
 
@@ -51,5 +57,86 @@ func main() {
 
 	if syncAuthMethodsErr != nil {
 		logrus.Fatal(syncAuthMethodsErr)
+	}
+
+	logical := targetClient.Logical()
+
+	for _, r := range config.TargetAuthAppRoles {
+		rolePath := fmt.Sprintf("auth/%s/role/%s", r.Path, r.Name)
+
+		exists, err := logical.Read(rolePath)
+
+		if err != nil {
+			logrus.Fatal(err)
+		}
+
+		if exists == nil {
+			_, err := logical.Write(rolePath, r.Options)
+
+			if err != nil {
+				logrus.Fatal(err)
+			}
+
+			var secretIDData map[string]interface{}
+
+			secretIDPath := fmt.Sprintf("%s/secret-id", rolePath)
+
+			secretIDSecret, secretIDErr := logical.Write(secretIDPath, secretIDData)
+
+			if secretIDErr != nil {
+				logrus.Fatal(secretIDErr)
+			}
+
+			roleIDPath := fmt.Sprintf("%s/role-id", rolePath)
+
+			roleIDSecret, err := logical.Read(roleIDPath)
+
+			if err != nil {
+				logrus.Fatal(err)
+			}
+
+			if config.TargetAuthAppRolesOutput != nil {
+				outputPath, err := filepath.Abs(*config.TargetAuthAppRolesOutput)
+
+				if err != nil {
+					logrus.Fatal(err)
+				}
+
+				if _, err := os.Stat(outputPath); os.IsNotExist(err) {
+					err := os.MkdirAll(outputPath, 0755)
+
+					if err != nil {
+						logrus.Fatal(err)
+					}
+				}
+
+				roleID := roleIDSecret.Data["role_id"]
+
+				secretID := secretIDSecret.Data["secret_id"]
+
+				approle := map[string]interface{}{
+					"role_id":   roleID,
+					"secret_id": secretID,
+				}
+
+				json, err := json.Marshal(approle)
+
+				if err != nil {
+					logrus.Fatal(err)
+				}
+
+				writePath := fmt.Sprintf("%s/%s.json", outputPath, r.Name)
+
+				writeErr := ioutil.WriteFile(writePath, json, 0644)
+
+				if writeErr != nil {
+					logrus.Fatal(writeErr)
+				}
+
+				fields := logrus.Fields{"role_id": roleID, "role_name": r.Name}
+
+				logrus.WithFields(fields).Info("Created AppRole in Vault")
+			}
+		}
 	}
 }
