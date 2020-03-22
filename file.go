1437664685f67a14d4b31335690e3eb126f1ed4c
Erik Reinert <e@drn.io>
2020-03-22 12:52:46 -0700

    #1: Implements sync policies logic from config

--- file.go
+++ file.go
@@ -2,11 +2,17 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
+	"os"
+	"path"
+	"path/filepath"
+	"strings"
 )
 
 // ConfigAuth : auth configuration
 type ConfigAuth struct {
+	Address     string            `json:"address"`
 	Credentials map[string]string `json:"credentials"`
 	Method      string            `json:"method"`
 }
@@ -22,15 +28,18 @@ type ConfigSecret struct {
 
 // Config : configuration for vaultsync
 type Config struct {
-	SourceAddr    string         `json:"vault_source_addr"`
-	SourceAuth    ConfigAuth     `json:"vault_source_auth"`
-	SourceSecrets []ConfigSecret `json:"vault_source_secrets"`
-	SourceToken   string         `json:"vault_source_token"`
-	TargetAddr    string         `json:"vault_target_addr"`
-	TargetToken   string         `json:"vault_target_token"`
+	SourceAuth         ConfigAuth     `json:"source_auth"`
+	SourceSecrets      []ConfigSecret `json:"source_secrets"`
+	SourcePoliciesPath string         `json:"source_policies_path"`
+	TargetAuth         ConfigAuth     `json:"target_auth"`
 }
 
-// GetConfig : gets configuration
+// FilenameWithoutExt : removes extension from filename
+func FilenameWithoutExt(name string) string {
+	return strings.TrimSuffix(name, path.Ext(name))
+}
+
+// GetConfig : gets configuration from path
 func GetConfig(path string) (*Config, error) {
 	data, err := ioutil.ReadFile(path)
 
@@ -49,4 +58,29 @@ func GetConfig(path string) (*Config, error) {
 	}
 
 	return &config, nil
+}
+
+// GetPolicies : gets all policies from path
+func GetPolicies(path string) ([]os.FileInfo, error) {
+	policies := make([]os.FileInfo, 0)
+
+	dir, err := ioutil.ReadDir(path)
+
+	if err != nil {
+		return nil, err
+	}
+
+	for _, f := range dir {
+		name := f.Name()
+
+		file := fmt.Sprintf("%s/%s", path, name)
+
+		extension := filepath.Ext(file)
+
+		if extension == ".hcl" {
+			policies = append(policies, f)
+		}
+	}
+
+	return policies, nil
 }
--- main.go
+++ main.go
@@ -17,21 +17,33 @@ func main() {
 		logrus.Fatal("invalid_config")
 	}
 
-	client, err := NewClient(config.TargetAddr, config.TargetToken)
+	targetToken, err := Login(config.TargetAuth)
 
 	if err != nil {
 		logrus.Fatal(err)
 	}
 
-	syncEnginesErr := SyncEngines(client, config)
+	targetClient, err := NewClient(config.TargetAuth.Address, *targetToken)
+
+	if err != nil {
+		logrus.Fatal(err)
+	}
+
+	syncEnginesErr := SyncEngines(targetClient, config)
 
 	if syncEnginesErr != nil {
 		logrus.Fatal(syncEnginesErr)
 	}
 
-	syncSecretsErr := SyncSecrets(client, config)
+	syncSecretsErr := SyncSecrets(targetClient, config)
 
 	if syncSecretsErr != nil {
 		logrus.Fatal(syncSecretsErr)
+	}
+
+	syncPoliciesErr := SyncPolicies(targetClient, config)
+
+	if syncEnginesErr != nil {
+		logrus.Fatal(syncPoliciesErr)
 	}
 }
